f5e899c5bab543ffa98583d204ea6d30524385b1
Mickael Mesquita <mickaelmendes50@gmail.com>
2018-05-29 01:12:50 +0000

    whyred: Add Google permissions
    Google permissions are required to enable Google Lens on
    Google Photos, the VR Mode and Pixel exclusive apps on Play
    Store
    
    Change-Id: I42f348e4a757f129c0c5016bd588baf049e6d399

--- device.mk
+++ device.mk
@@ -60,6 +60,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Google Permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/pixel_2017_exclusive.xml:system/etc/permissions/pixel_2017_exclusive.xml \
+    $(LOCAL_PATH)/permissions/pixel_2017.xml:system/etc/permissions/pixel_2017.xml \
+    $(LOCAL_PATH)/permissions/nexus.xml:system/etc/permissions/nexus.xml \
+    $(LOCAL_PATH)/permissions/google_build.xml:system/etc/permissions/google_build.xml
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
