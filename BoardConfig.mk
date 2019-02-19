946a4eaf89098520c417c4c45b10a6e0130ae56d
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-19 00:02:53 -0300

    whyred: Initial changes for caf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -229,7 +229,6 @@ VENDOR_SECURITY_PATCH := 2019-01-01
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Treble
